dbad3a7ab77742f64945284c84318da088495d6f
fz3hra <mujorefaatimah@gmail.com>
2025-02-09 01:21:49 +0400

    remove user dto from post service

--- Post.API/ModelDtos/UserDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Post.API.ModelDtos;
-
-public class UserDto
-{
-    public string Id { get; set; }
-    public string Email { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-}
