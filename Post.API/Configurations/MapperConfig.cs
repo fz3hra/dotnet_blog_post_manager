97bb983c0700456eeb91adfb24a0c14e1e02a0ad
fz3hra <mujorefaatimah@gmail.com>
2025-02-10 09:34:37 +0400

    fix update req

--- Post.API/Configurations/MapperConfig.cs
+++ Post.API/Configurations/MapperConfig.cs
@@ -10,6 +10,9 @@ public class MapperConfig: Profile
         CreateMap<Data.Post, GetPostDto>().ReverseMap();
         
         CreateMap<Data.Post, UpdatePostDto>().ReverseMap();
+        CreateMap<UpdatePostDto, Data.Post>()
+            .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
         
         CreateMap<CreatePostDto, Data.Post>().ReverseMap();
         CreateMap<Data.Post, PostResponse>();
--- Post.API/ModelDtos/UpdatePostDto.cs
+++ Post.API/ModelDtos/UpdatePostDto.cs
@@ -4,5 +4,6 @@ namespace Post.API.ModelDtos;
 
 public class UpdatePostDto : BasePostDto
 {
+    public bool IsPublished { get; set; } = false;
     public PostStatus Status { get; set; }
 }
